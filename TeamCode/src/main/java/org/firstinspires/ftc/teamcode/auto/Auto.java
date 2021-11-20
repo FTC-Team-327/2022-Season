f699e3e5530091d6a73dee29d038bf2548220ff8
The1TrueJoe <jttelaak@hotmail.com>
2021-11-20 10:44:49 -0500

    Add auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Auto.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous
+
+public class Auto {
+
+	// todo: write your code here
+}
