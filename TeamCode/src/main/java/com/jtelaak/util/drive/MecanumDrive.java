a0785e248fa221a9191507e29fbb018aa551ceb2
The1TrueJoe <jttelaak@hotmail.com>
2021-11-20 11:05:19 -0500

    Fix mecanum

--- TeamCode/src/main/java/com/jtelaak/util/drive/MecanumDrive.java
+++ TeamCode/src/main/java/com/jtelaak/util/drive/MecanumDrive.java
@@ -32,54 +32,54 @@ public class MecanumDrive extends Drive {
     }
 
 
-/**
- * Heavily inspired by https://github.com/brandon-gong/ftc-mecanum
- * 
- * takes in values from -1 to 1, for 3 axes.
- * 
- * @param front_back forward/backward motion
- * @param strafe left/right motion
- * @param rotate rotation
- * 
- */
+    /**
+     * Heavily inspired by https://github.com/brandon-gong/ftc-mecanum
+     * 
+     * takes in values from -1 to 1, for 3 axes.
+     * 
+     * @param front_back forward/backward motion
+     * @param strafe left/right motion
+     * @param rotate rotation
+     * 
+     */
 
-public void mecdrive(double front_back, double strafe, double rotate) {
+    public void mecdrive(double front_back, double strafe, double rotate) {
 
-    // Mecanum drive is controlled with three axes: drive (front-and-back),
-    // strafe (left-and-right), and twist (rotating the whole chassis).
-    double drive  = front_back;
-    double strafe = left_stick_x;
-    double twist  = right_stick_x;
+        // Mecanum drive is controlled with three axes: drive (front-and-back),
+        // strafe (left-and-right), and twist (rotating the whole chassis).
 
-    // You may need to multiply some of these by -1 to invert direction of
-    // the motor.  This is not an issue with the calculations themselves.
-    double[] speeds = {
-        (drive + strafe + twist),
-        (drive - strafe - twist),
-        (drive - strafe + twist),
-        (drive + strafe - twist)
-    };
+        // You may need to multiply some of these by -1 to invert direction of
+        // the motor.  This is not an issue with the calculations themselves.
+        double[] speeds = {
+            (front_back + strafe + rotate),
+            (front_back - strafe - rotate),
+            (front_back - strafe + rotate),
+            (front_back + strafe - rotate)
+        };
 
-    // Because we are adding vectors and motors only take values between
-    // [-1,1] we may need to normalize them.
+        // Because we are adding vectors and motors only take values between
+        // [-1,1] we may need to normalize them.
 
-    // Loop through all values in the speeds[] array and find the greatest
-    // *magnitude*.  Not the greatest velocity.
-    double max = Math.abs(speeds[0]);
-    for(int i = 0; i < speeds.length; i++) {
-        if ( max < Math.abs(speeds[i]) ) max = Math.abs(speeds[i]);
+        // Loop through all values in the speeds[] array and find the greatest
+        // *magnitude*.  Not the greatest velocity.
+        double max = Math.abs(speeds[0]);
+        for(int i = 0; i < speeds.length; i++) {
+            if ( max < Math.abs(speeds[i]) ) max = Math.abs(speeds[i]);
+
+        }
+
+        // If and only if the maximum is outside of the range we want it to be,
+        // normalize all the other speeds based on the given speed value.
+        if (max > 1) {
+            for (int i = 0; i < speeds.length; i++) speeds[i] /= max;
+        }
+
+        // apply the calculated values to the motors.
+        motors[FRONT_LEFT_MOTOR].setPower(speeds[0]);
+        motors[FRONT_RIGHT_MOTOR].setPower(speeds[1]);
+        motors[REAR_LEFT_MOTOR].setPower(speeds[2]);
+        motors[REAR_RIGHT_MOTOR].setPower(speeds[3]);
+
     }
 
-    // If and only if the maximum is outside of the range we want it to be,
-    // normalize all the other speeds based on the given speed value.
-    if (max > 1) {
-        for (int i = 0; i < speeds.length; i++) speeds[i] /= max;
-    }
-
-    // apply the calculated values to the motors.
-    front_left.setPower(speeds[0]);
-    front_right.setPower(speeds[1]);
-    back_left.setPower(speeds[2]);
-    back_right.setPower(speeds[3]);
-}
 }
