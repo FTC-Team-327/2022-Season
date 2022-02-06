2f7f9c1577eee660e97658137d96c8fa8e56b131
The1TrueJoe <jttelaak@hotmail.com>
2022-02-06 18:31:46 -0500

    Telemetry and motor config

--- RobotV2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OurRobot.java
+++ RobotV2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OurRobot.java
@@ -12,113 +12,117 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
  */
 
 public class OurRobot {
-    
-    // ------ FTC Controller 
-    
-    /** Telemetry info */
-    public Telemetry telemetry;
-    
-    /** Hardware Map */
-    public HardwareMap hardware_map;
-    
-    // ------ Robot Hardware
-    
-    /** Drive Train */
-    public DriveTrain drivetrain;
-    
-    /** Spinner */
-    public Spinner spinner;
-    
-    // ------ Init
-    
-    /**
-     * Robot Constructor
-     * 
-     * @param telemetry Telemetry object from opmode
-     */
+	
+	// ------ FTC Controller 
+	
+	/** Telemetry info */
+	public Telemetry telemetry;
+	
+	/** Hardware Map */
+	public HardwareMap hardware_map;
+	
+	// ------ Robot Hardware
+	
+	/** Drive Train */
+	public DriveTrain drivetrain;
+	
+	/** Spinner */
+	public Spinner spinner;
+	
+	// ------ Init
+	
+	/**
+	 * Robot Constructor
+	 * 
+	 * @param telemetry Telemetry object from opmode
+	 */
 
-    public OurRobot(Telemetry telemetry) {
-        // Pass through telemetry
-        this.telemetry = telemetry;
-        
-        // Post update
-        telemetry.addData("Status", "Hardware Object Created");
-        telemetry.update();
-        
-    }
-    
-    /**
-     * Initialize robot hardware
-     * 
-     * @param hardware_map Hardware mapper from opmode
-     */
-    
-    public void init(HardwareMap hardware_map) {
-        // Post update
-        telemetry.addData("Status", "Initializing");
-        telemetry.update();
-        
-        // Hardware Map
-        this.hardware_map = hardware_map;
-        
-        // Initialize DriveTrain
-        initDrivetrain();
-        
-        // Initialize Spinner
-        initSpinner();
-        
-        // Post update
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-        
-    }
-    
-    /**
-     * Initialize robot hardware
-     * 
-     * @param hardware_map Hardware mapper from opmode
-     */
-    
-    public void init_driveonly(HardwareMap hardware_map) {
-        // Post update
-        telemetry.addData("Status", "Initializing");
-        telemetry.update();
-        
-        // Hardware Map
-        this.hardware_map = hardware_map;
-        
-        // Initialize DriveTrain
-        initDrivetrain();
-        
-        // Post update
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-        
-    }
-    
-    /**
-     * Initialize Drivetrain
-     */
-    
-    private void initDrivetrain() {
-        // Setup Drivetrain
-        drivetrain = new DriveTrain(new DcMotor[] {
-            hardware_map.get(DcMotor.class, Constants.front_left_motor_name),
-            hardware_map.get(DcMotor.class, Constants.front_right_motor_name),
-            hardware_map.get(DcMotor.class, Constants.rear_left_motor_name),
-            hardware_map.get(DcMotor.class, Constants.rear_right_motor_name)
-            
-        });
-    }
-    
-    /**
-     * Initialize Spinner
-     */
-     
-    private void initSpinner() {
-        // Spinner
-        spinner = new Spinner(hardware_map.get(DcMotor.class, Constants.spinner_motor_name));
-        
-    }
+	public OurRobot(Telemetry telemetry) {
+		// Pass through telemetry
+		this.telemetry = telemetry;
+		
+		// Post update
+		telemetry.addData("Status", "Hardware Object Created");
+		telemetry.update();
+		
+	}
+	
+	/**
+	 * Initialize robot hardware
+	 * 
+	 * @param hardware_map Hardware mapper from opmode
+	 */
+	
+	public void init(HardwareMap hardware_map) {
+		// Post update
+		telemetry.addData("Status", "Initializing");
+		telemetry.update();
+		
+		// Hardware Map
+		this.hardware_map = hardware_map;
+		
+		// Initialize DriveTrain
+		initDrivetrain();
+		
+		// Initialize Spinner
+		initSpinner();
+		
+		// Post update
+		telemetry.addData("Status", "Initialized");
+		telemetry.update();
+		
+	}
+	
+	/**
+	 * Initialize robot hardware
+	 * 
+	 * @param hardware_map Hardware mapper from opmode
+	 */
+	
+	public void init_driveonly(HardwareMap hardware_map) {
+		// Post update
+		telemetry.addData("Status", "Initializing");
+		telemetry.update();
+		
+		// Hardware Map
+		this.hardware_map = hardware_map;
+		
+		// Initialize DriveTrain
+		initDrivetrain();
+		
+		// Post update
+		telemetry.addData("Status", "Initialized");
+		telemetry.update();
+		
+	}
+	
+	/**
+	 * Initialize Drivetrain
+	 */
+	
+	private void initDrivetrain() {
+		// Setup Drivetrain
+		drivetrain = new DriveTrain(
+			new DcMotor[] {
+				hardware_map.get(DcMotor.class, Constants.front_left_motor_name),
+				hardware_map.get(DcMotor.class, Constants.front_right_motor_name),
+				hardware_map.get(DcMotor.class, Constants.rear_left_motor_name),
+				hardware_map.get(DcMotor.class, Constants.rear_right_motor_name)
+			
+			},
+			telemetry
+			
+		);
+	}
+	
+	/**
+	 * Initialize Spinner
+	 */
+	 
+	private void initSpinner() {
+		// Spinner
+		spinner = new Spinner(hardware_map.get(DcMotor.class, Constants.spinner_motor_name));
+		
+	}
 
 }
--- RobotV2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveTrain.java
+++ RobotV2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveTrain.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 /**
@@ -9,191 +10,225 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class DriveTrain {
 
-    // --------------------- Mec Drive Values
+	// --------------------- Motor array indexes
 
-    public double forward;
-    public double strafe;
-    public double rotate;
+	/** Front Left Dc_Motor */
+	public final int FRONT_LEFT_MOTOR = 0;
+	
+	/** Front Right Dc_Motor */
+	public final int FRONT_RIGHT_MOTOR = 1;
+	
+	/** Rear Left Dc_Motor */
+	public final int REAR_LEFT_MOTOR = 2;
+	
+	/** Rear Right Dc_Motor */
+	public final int REAR_RIGHT_MOTOR = 3;
 
-    // --------------------- Motor array indexes
+	// --------------------- Motor objects
 
-    /** Front Left Dc_Motor */
-    public final int FRONT_LEFT_MOTOR = 0;
-    
-    /** Front Right Dc_Motor */
-    public final int FRONT_RIGHT_MOTOR = 1;
-    
-    /** Rear Left Dc_Motor */
-    public final int REAR_LEFT_MOTOR = 2;
-    
-    /** Rear Right Dc_Motor */
-    public final int REAR_RIGHT_MOTOR = 3;
+	/** Motors */
+	public DcMotor[] motors;
+	
+	// --------------------- Telemetry
 
-    // --------------------- Motor objects
+	/** Telemetry */
+	public Telemetry telemetry;
 
-    /** Motors */
-    public DcMotor[] motors;
+	// --------------------- Constructors
 
-    // --------------------- Constructors
+	/**
+	 * Drive constructor (See public ints for indexes)
+	 * 
+	 * @param motors Motor array
+	 */
 
-    /**
-     * Drive constructor (See public ints for indexes)
-     * 
-     * @param motors Motor array
-     */
+	public DriveTrain(DcMotor[] motors, Telemetry telemetry) {
+		// Set the global motor list
+		this.motors = motors;
+		
+		// Telemetry
+		this.telemetry = telemetry;
 
-    public DriveTrain(DcMotor[] motors) {
-        // Set the global motor list
-        this.motors = motors;
+		// Config motors
+		configureMotors();
 
-        // Set every other motor to forwards
-        for (int i = 0; i < motors.length; i++) {
-            if (i % 2 == 0) {
-                motors[i].setDirection(DcMotor.Direction.FORWARD);
+	}
+	
+	/**
+	 * Drive constructor (See public ints for indexes)
+	 * 
+	 * @param motor_ids Motor ids from hardware map
+	 * @param hardware_map Hardware mapper
+	 */
 
-            }
-        }
+	public DriveTrain(String[] motors, HardwareMap hardware_map, Telemetry telemetry) {
+		// Set the global motor list
+		this.motors = new DcMotor[motors.length];
+		
+		// Telemetry
+		this.telemetry = telemetry;
+		
+		// Map all motors
+		for (int i = 0; i < motors.length; i++) {
+			this.motors[i] = hardware_map.get(DcMotor.class, motors[i]);
+			
+		}
 
-        // Set every other motor to revers
-        for (int i = 0; i < motors.length; i++) {
-            if (i % 2 == 1) {
-                motors[i].setDirection(DcMotor.Direction.REVERSE);
+		// Config motors
+		configureMotors();
 
-            }
-        }
+	}
 
-        // Zero power behavior
-        for (DcMotor motor : motors) {
-            motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+	/**
+	 * Drive constructor
+	 * 
+	 * @param front_left_motor front left motor
+	 * @param front_right_motor front right motor
+	 * @param rear_left_motor rear left motor
+	 * @param rear_right_motor rear right motor
+	 * @param telemetry telemetry
+	 */
 
-        }
+	public DriveTrain(DcMotor front_left_motor, DcMotor front_right_motor, DcMotor rear_left_motor, DcMotor rear_right_motor, Telemetry telemetry) {
+		// Telemetry
+		this.telemetry = telemetry;
+		
+		// Motor setup
+		motors = new DcMotor[4];
+		
+		// Set motors
+		motors[FRONT_LEFT_MOTOR] = front_left_motor;
+		motors[FRONT_LEFT_MOTOR] = front_right_motor;
+		motors[REAR_LEFT_MOTOR]  = rear_left_motor;
+		motors[REAR_RIGHT_MOTOR] = rear_right_motor;
+		
+		// Config motors
+		configureMotors();
 
-        //resetEncoders();
-        //enableEncoders();
+	}
+	
+	/**
+	 * Configure the motors (called from instructor)
+	 */
+	
+	private void configureMotors() {
+		// Set every other motor to forwards
+		for (int i = 0; i < motors.length; i++) {
+			if (i % 2 == 0) {
+				motors[i].setDirection(DcMotor.Direction.FORWARD);
 
-    }
-    
-    /**
-     * Drive constructor (See public ints for indexes)
-     * 
-     * @param motor_ids Motor ids from hardware map
-     * @param hardware_map Hardware mapper
-     */
+			}
+		}
 
-    public DriveTrain(String[] motors, HardwareMap hardware_map) {
-        // Set the global motor list
-        DcMotors defined_motors = new DcMotor[motors.length];
-        
-        for (int i = 0; i < motors.length; i++) {
-            defined_motors[i] = hardware_map.get(DcMotos.class, motors[i]);
-            
-        }
+		// Set every other motor to revers
+		for (int i = 0; i < motors.length; i++) {
+			if (i % 2 == 1) {
+				motors[i].setDirection(DcMotor.Direction.REVERSE);
 
-        this(defined_motors);
+			}
+		}
 
-    }
+		// Zero power behavior
+		for (DcMotor motor : motors) {
+			motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-    /**
-     * Drive constructor
-     * 
-     * @param front_left_motor front left motor
-     * @param front_right_motor front right motor
-     * @param rear_left_motor rear left motor
-     * @param rear_right_motor rear right motor
-     */
+		}
+	}
 
-    public DriveTrain(DcMotor front_left_motor, DcMotor front_right_motor, DcMotor rear_left_motor, DcMotor rear_right_motor) {
-        motors = new DcMotor[4];
-        
-        motors[FRONT_LEFT_MOTOR] = front_left_motor;
-        motors[FRONT_LEFT_MOTOR] = front_right_motor;
-        motors[REAR_LEFT_MOTOR]  = rear_left_motor;
-        motors[REAR_RIGHT_MOTOR] = rear_right_motor;
+	/**
+	 * Brake
+	 */
 
-    }
+	public void brake() {
+		for (DcMotor motor : motors) {
+			motor.setPower(0);
 
-    /**
-     * Brake
-     */
+		}
+	}
 
-    public void brake() {
-        for (DcMotor motor : motors) {
-            motor.setPower(0);
+	/**
+	 * Drive
+	 * 
+	 * @param motor Motor index to control
+	 * 
+	 */
 
-        }
-    }
+	public void setPower(int motor, double power) {
+		motors[motor].setPower(power);
+		telemetry.addData("Motor " + motor, "Power " + power);
 
-    /**
-     * Drive
-     * 
-     * @param motor Motor index to control
-     * 
-     */
+	}
 
-    public void setPower(int motor, double power) {
-        motors[motor].setPower(power);
+	/**
+	 * Simple drive 
+	 * 
+	 * @param left left motor power (left motors have even index)
+	 * @param right right motor power (right motors have even index)
+	 */
 
-    }
+	public void tankDrive(double left, double right) {
+		for (int i = 0; i < motors.length; i++) {
+			if (i % 2 == 0) {
+				setPower(i, left);
+				
+			}
+		}
 
-    /**
-     * Simple drive 
-     * 
-     * @param left left motor power (left motors have even index)
-     * @param right right motor power (right motors have even index)
-     */
+		for (int i = 0; i < motors.length; i++) {
+			if (i % 2 == 1) {
+				setPower(i, right);
+				
+			}
+		}
+		
+		telemetry.update();
+		
+	}
 
-    public void tankDrive(double left, double right) {
-        for (int i = 0; i < motors.length; i++) {
-            if (i % 2 == 0) {
-                setPower(i, left);
-            }
-        }
+	/**
+	 * takes in values from -1 to 1, for 3 axes.
+	 * 
+	 * @param front_back forward/backward motion
+	 * @param strafe left/right motion
+	 * @param rotate rotation
+	 * 
+	 */
+	 
+	public void mecDrive(double forward, double strafe, double rotate) {
+		// strafe is reversed
+		strafe = strafe * -1;
 
-        for (int i = 0; i < motors.length; i++) {
-            if (i % 2 == 1) {
-                setPower(i, right);
-            }
-        }
+		double[] speeds = {
+			(forward + strafe + rotate),
+			(forward - strafe - rotate),
+			(forward - strafe + rotate),
+			(forward + strafe - rotate)
+		};
 
-    }
+		
+		double max = Math.abs(speeds[0]);
+		for(int i = 0; i < speeds.length; i++) {
+			if ( max < Math.abs(speeds[i]) ) max = Math.abs(speeds[i]);
 
-    /**
-     * takes in values from -1 to 1, for 3 axes.
-     * 
-     * @param front_back forward/backward motion
-     * @param strafe left/right motion
-     * @param rotate rotation
-     * 
-     */
+		}
 
-    public void mecDrive(double forward, double strafe, double rotate) {
-        // strafe is reversed
-        strafe = strafe * -1;
+		if (max > 1) {
+			for (int i = 0; i < speeds.length; i++) speeds[i] /= max;
+		}
 
-        double[] speeds = {
-            (forward + strafe + rotate),
-            (forward - strafe - rotate),
-            (forward - strafe + rotate),
-            (forward + strafe - rotate)
-        };
+		// apply the calculated values to the motors.
+		for (int i = 0; i < motors.length; i++) {
+			setPower(i, speeds[i]);
+			
+		}
+		
+		telemetry.update();
+		
+		//motors[FRONT_LEFT_MOTOR].setPower(speeds[0]);
+		//motors[FRONT_RIGHT_MOTOR].setPower(speeds[1]);
+		//motors[REAR_LEFT_MOTOR].setPower(speeds[2]);
+		//motors[REAR_RIGHT_MOTOR].setPower(speeds[3]);
 
-        
-        double max = Math.abs(speeds[0]);
-        for(int i = 0; i < speeds.length; i++) {
-            if ( max < Math.abs(speeds[i]) ) max = Math.abs(speeds[i]);
-
-        }
-
-        if (max > 1) {
-            for (int i = 0; i < speeds.length; i++) speeds[i] /= max;
-        }
-
-        // apply the calculated values to the motors.
-        motors[FRONT_LEFT_MOTOR].setPower(speeds[0]);
-        motors[FRONT_RIGHT_MOTOR].setPower(speeds[1]);
-        motors[REAR_LEFT_MOTOR].setPower(speeds[2]);
-        motors[REAR_RIGHT_MOTOR].setPower(speeds[3]);
-
-    }
-    
+	}
+	
 }
