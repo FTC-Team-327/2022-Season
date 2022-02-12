84a2218de5fc03aad08a29894bd96463777b96b4
Joseph Telaak <47280152+The1TrueJoe@users.noreply.github.com>
2022-02-12 14:29:20 -0500

    Add scoop to elevator

--- RobotV2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Elevator.java
+++ RobotV2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Elevator.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.subsystems;
 import org.firstinspires.ftc.teamcode.Constants;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 /**
@@ -18,16 +19,21 @@ public class Elevator {
     /** Motor for the Elevator  */
     private DcMotor motor;
 
+    /** Servo for the scoop  */
+    private Servo scoop;
+
     // ---------------- Constructors
 
     /**
      * Constructor
      * 
      * @param motor Motor to assign to the Elevator
+     * @param servo Servo to assign to the scoop
      */
 
-    public Elevator(DcMotor motor) {
+    public Elevator(DcMotor motor, Servo servo) {
         this.motor = motor;
+        this.scoop = scoop;
 
         this.motor.setDirection(DcMotor.Direction.REVERSE);
 
@@ -36,12 +42,14 @@ public class Elevator {
     /**
      * Constructor
      * 
-     * @param motor Name of the motor from the robot configuration
+     * @param motor Name of the motor from the robot configuration for the elevator
+     * @param servo Name of the servo from the robot configuration for the scoop
      * @param hardware_map Maps the hardware given the string name of the motor
      */
 
-    public Elevator(String motor, HardwareMap hardware_map) {
+    public Elevator(String motor, String servo, HardwareMap hardware_map) {
         this.motor = hardware_map.get(DcMotor.class, motor);
+        this.scoop = hardware_map.get(Servo.class, servo);
 
         this.motor.setDirection(DcMotor.Direction.REVERSE);
 
@@ -57,6 +65,7 @@ public class Elevator {
 
     public Elevator(HardwareMap hardware_map) {
         this.motor = hardware_map.get(DcMotor.class, Constants.elevator_motor_name);
+        this.scoop = hardware_map.get(DcMotor.class, Constants.scoop_servo_name);
 
         this.motor.setDirection(DcMotor.Direction.REVERSE);
 
@@ -68,7 +77,7 @@ public class Elevator {
      * Reverses the motor direction
      */
 
-    public void reverseDirection() {
+    public void reverseElevatorDirection() {
         motor.setDirection(DcMotor.Direction.REVERSE);
 
     }
@@ -77,7 +86,7 @@ public class Elevator {
      * Un-reverses motor direction
      */
 
-    public void forwardDirection() {
+    public void forwardElevatorDirection() {
         motor.setDirection(DcMotor.Direction.FORWARD);
 
     }
@@ -106,6 +115,35 @@ public class Elevator {
 
     public void stopElevator() {
         motor.setPower(0);
+
+    }
+
+     /**
+     * Run the Intake at max speed
+     */
+
+    public void runIntake(double power) {
+        intake.setPower(power);
+
+    }
+
+     /**
+     * Run the Intake
+     * 
+     * @param power Power (-1, 1) to run motor
+     */
+
+    public void runIntake() {
+        intake.setPower();
+
+    }
+
+    /**
+     * Stop the intake
+     */
+
+    public void stopIntake() {
+        intake.setPower(0);
 
     }
 
