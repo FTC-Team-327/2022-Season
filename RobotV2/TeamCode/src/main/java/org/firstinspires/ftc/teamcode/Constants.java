d103ec8114dd49ea8db29f284d8d36b03f94a909
Joseph Telaak <47280152+The1TrueJoe@users.noreply.github.com>
2022-02-13 14:04:59 -0500

    Update constants

--- RobotV2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ RobotV2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -5,97 +5,97 @@ package org.firstinspires.ftc.teamcode;
  */
 
 public class Constants {
-    
-    // -------------------- Drivetrain
+	
+	// -------------------- Drivetrain
 
-    /** Front Left Drivetrain motor */
-    public static final String front_left_motor_name = "front_left";
-    
-    /** Front Right Drivetrain motor */
-    public static final String front_right_motor_name = "front_right";
-    
-    /** Rear Left Drivetrain motor */
-    public static final String rear_left_motor_name = "back_left";
-    
-    /** Rear Right Drivetrain motor */
-    public static final String rear_right_motor_name = "back_right";
-    
-    
-    
+	/** Front Left Drivetrain motor */
+	public static final String front_left_motor_name = "front_left";
+	
+	/** Front Right Drivetrain motor */
+	public static final String front_right_motor_name = "front_right";
+	
+	/** Rear Left Drivetrain motor */
+	public static final String rear_left_motor_name = "back_left";
+	
+	/** Rear Right Drivetrain motor */
+	public static final String rear_right_motor_name = "back_right";
+	
+	
+	
 
-    // -------------------- Chassis Sensors
-    
-    // ----------- Front Range Sensor
+	// -------------------- Chassis Sensors
+	
+	// ----------- Front Range Sensor
 
-    /** Chassis Front Range Sensor */
-    public static final String chassis_front_range_name = "front_range";
-    
-    /** Chassis Front Range Sensor Offset */
-    public static final double chassis_front_range_offset = 0.0;
-    
-    /** Chassis Front Range Sensor Tolerance */
-    public static final double chassis_front_range_tolerance = 0.0;
-    
-    
-    // ----------- Right Range Sensor
-    
-    /** Chassis Left Range Sensor */
-    public static final String chassis_right_range_name = "right_range";
-    
-    /** Chassis Right Range Sensor Offset */
-    public static final double chassis_right_range_offset = 0.0;
-    
-    /** Chassis Right Range Sensor Tolerance */
-    public static final double chassis_right_range_tolerance = 0.0;
-    
-    
-    // ----------- Left Range Sensor
-    
-    /** Chassis Left Range Sensor */
-    public static final String chassis_left_range_name = "left_range";
-    
-    /** Chassis Left Range Sensor Offset */
-    public static final double chassis_left_range_offset = 0.0;
-    
-    /** Chassis Left Range Sensor Tolerance */
-    public static final double chassis_left_range_tolerance = 0.0;
-    
-    
-    
+	/** Chassis Front Range Sensor */
+	public static final String chassis_front_range_name = "front_range";
+	
+	/** Chassis Front Range Sensor Offset */
+	public static final double chassis_front_range_offset = 0.0;
+	
+	/** Chassis Front Range Sensor Tolerance */
+	public static final double chassis_front_range_tolerance = 0.0;
+	
+	
+	// ----------- Right Range Sensor
+	
+	/** Chassis Left Range Sensor */
+	public static final String chassis_right_range_name = "right_range";
+	
+	/** Chassis Right Range Sensor Offset */
+	public static final double chassis_right_range_offset = 0.0;
+	
+	/** Chassis Right Range Sensor Tolerance */
+	public static final double chassis_right_range_tolerance = 0.0;
+	
+	
+	// ----------- Left Range Sensor
+	
+	/** Chassis Left Range Sensor */
+	public static final String chassis_left_range_name = "left_range";
+	
+	/** Chassis Left Range Sensor Offset */
+	public static final double chassis_left_range_offset = 0.0;
+	
+	/** Chassis Left Range Sensor Tolerance */
+	public static final double chassis_left_range_tolerance = 0.0;
+	
+	
+	
 
-    // -------------------- Spinner
+	// -------------------- Spinner
 
-    /** Spinner Motor */
-    public static final String spinner_motor_name = "spinner_motor";
-    
-    /** Spinner Range Sensor */
-    public static final String spinner_range_sensor_name = "spinner_range";
-    
-    /** Spinner Range Sensor Offset */
-    public static final double spinner_range_sensor_offset = 0.0;
-    
-    /** Spinner Range Sensor Tolerance */
-    public static final double spinner_range_sensor_tolerance = 0.0;
-
-
-
-    // -------------------- Elevator 
-
-    /** Elevator Slider Motor */
-    public static final String elevator_motor_name = "slider";
-
-    /** Elevator Slider Motor */
-    public static final String elevator_bottom_limit_switch_name = "slider_bottom_limit";
-
-    /** Elevator Scoop Servo */
-    public static final String scoop_servo_name = "scoop";
+	/** Spinner Motor */
+	public static final String spinner_motor_name = "spinner_motor";
+	
+	/** Spinner Range Sensor */
+	public static final String spinner_range_sensor_name = "spinner_range";
+	
+	/** Spinner Range Sensor Offset */
+	public static final double spinner_range_sensor_offset = 0.0;
+	
+	/** Spinner Range Sensor Tolerance */
+	public static final double spinner_range_sensor_tolerance = 0.0;
 
 
 
-    // -------------------- Intake 
+	// -------------------- Elevator 
 
-    /** Intake Motor */
-    public static final String intake_motor_name = "intake";
+	/** Elevator Slider Motor */
+	public static final String elevator_motor_name = "slider";
 
-    
+	/** Elevator Slider Motor */
+	public static final String elevator_bottom_limit_switch_name = "slider_bottom_limit";
+
+	/** Elevator Scoop Servo */
+	public static final String scoop_servo_name = "scoop";
+
+
+
+	// -------------------- Intake 
+
+	/** Intake Motor */
+	public static final String intake_motor_name = "intake";
+
+	
 }
