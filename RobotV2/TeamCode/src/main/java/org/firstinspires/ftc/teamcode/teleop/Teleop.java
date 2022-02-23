88bee33e35d174332bc90216f4e865416278b9b6
The1TrueJoe <jttelaak@hotmail.com>
2022-02-22 20:25:40 -0500

    Dual controllers

--- RobotV2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Teleop.java
+++ RobotV2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Teleop.java
@@ -33,38 +33,39 @@ public class Teleop extends LinearOpMode {
 			// Run
 			double x_multiplier = 1;
 			double y_multiplier = 1;
-			
-			// Driver gamepad speed modifier
-			if (gamepad1.left_trigger > (long) 0.10)  { x_multiplier = 2; }
-			if (gamepad1.right_trigger > (long) 0.10) { y_multiplier = 2; }
 
 			// Drive speeds
-			forward =   (gamepad1.left_stick_y) * y_multiplier;
-			strafe  =	(gamepad1.left_stick_x + gamepad1.left_trigger - gamepad1.right_trigger) * x_multiplier;
-			rotate  =	(gamepad1.right_stick_x) * -1;
+			forward =   (gamepad1.left_stick_y + gamepad2.left_stick_y) * y_multiplier;
+			strafe  =	(
+							(gamepad1.left_stick_x + gamepad1.left_trigger - gamepad1.right_trigger) +
+							(gamepad2.left_stick_x + gamepad2.left_trigger - gamepad2.right_trigger)
+
+						) * x_multiplier;
+
+			rotate  =	(gamepad1.right_stick_x + gamepad2.right_stick_x) * -1;
 
 			// Drive
 			robot.drivetrain.mecDrive(forward, strafe, rotate);
 
 			// Intake
-			if (gamepad1.dpad_up) { robot.intake.runIntake(1) }
-			if (gamepad1.dpad_down) { robot.intake.runIntake(-1) }
+			if (gamepad1.dpad_up || gamepad2.dpad_up) { robot.intake.runIntake(1) }
+			if (gamepad1.dpad_down || gamepad2.dpad_down) { robot.intake.runIntake(-1) }
 
 			// Topper
-			if (gamepad1.dpad_left) { robot.topper.runTopper(1.0); }
-			if (gamepad1.dpad_right) { robot.topper.runTopper(-1.0); }
+			if (gamepad1.dpad_left || gamepad2.dpad_left) { robot.topper.runTopper(1.0); }
+			if (gamepad1.dpad_right || gamepad2.dpad_right) { robot.topper.runTopper(-1.0); }
 
 			// Elevator
-			if (gamepad1.a) { robot.elevator.runElevator(1); }
-			if (gamepad1.y) { robot.elevator.runElevator(-1); }
+			if (gamepad1.a || gamepad2.a) { robot.elevator.runElevator(1); }
+			if (gamepad1.y || gamepad2.y) { robot.elevator.runElevator(-1); }
 
 			// Scoop
-			if (gamepad1.x) { robot.elevator.setScoopPos(0); }
-			if (gamepad1.b) { robot.elevator.setScoopPos(180); }
+			if (gamepad1.x || gamepad2.x) { robot.elevator.setScoopPos(0); }
+			if (gamepad1.b || gamepad2.b) { robot.elevator.setScoopPos(180); }
 
 			// Spinner
-			if (gamepad1.left_bumper) { robot.spinner.runSpinner(1); }
-			if (gamepad1.right_bumper) { robot.spinner.runSpinner(-1); }
+			if (gamepad1.left_bumper || gamepad2.left_bumper) { robot.spinner.runSpinner(1); }
+			if (gamepad1.right_bumper || gamepad2.right_bumper) { robot.spinner.runSpinner(-1); }
 
 			
 			// Update telemetry
